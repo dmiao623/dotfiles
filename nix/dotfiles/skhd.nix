3db5b838ea9b72487e6e5a0fdf79e2a82b0ccb09
dustinm <dmiao0107@gmail.com>
2025-05-25 19:26:11 -0700

    minor formatting changes

--- nix/dotfiles/skhd.nix
+++ nix/dotfiles/skhd.nix
@@ -1,46 +1,49 @@
 { config, lib, pkgs, ... }:
 
+let
+  yabaiBin = "/opt/homebrew/bin/yabai";
+in
 {
   services.skhd = {
     enable = true;
 
     config = ''
-      alt - j : /opt/homebrew/bin/yabai -m window --focus south
-      alt - k : /opt/homebrew/bin/yabai -m window --focus north
-      alt - h : /opt/homebrew/bin/yabai -m window --focus west
-      alt - l : /opt/homebrew/bin/yabai -m window --focus east
+      alt - j : ${yabaiBin} -m window --focus south
+      alt - k : ${yabaiBin} -m window --focus north
+      alt - h : ${yabaiBin} -m window --focus west
+      alt - l : ${yabaiBin} -m window --focus east
 
-      shift + alt - r : /opt/homebrew/bin/yabai -m space --rotate 270
-      shift + alt - y : /opt/homebrew/bin/yabai -m space --mirror y-axis
-      shift + alt - x : /opt/homebrew/bin/yabai -m space --mirror x-axis
-      shift + alt - t : /opt/homebrew/bin/yabai -m window --toggle float --grid 4:4:1:1:2:2
+      shift + alt - r : ${yabaiBin} -m space --rotate 270
+      shift + alt - y : ${yabaiBin} -m space --mirror y-axis
+      shift + alt - x : ${yabaiBin} -m space --mirror x-axis
+      shift + alt - t : ${yabaiBin} -m window --toggle float --grid 4:4:1:1:2:2
 
-      shift + alt - m : /opt/homebrew/bin/yabai -m window --toggle zoom-fullscreen
-      shift + alt - e : /opt/homebrew/bin/yabai -m space --balance
+      shift + alt - m : ${yabaiBin} -m window --toggle zoom-fullscreen
+      shift + alt - e : ${yabaiBin} -m space --balance
 
-      shift + alt - j : /opt/homebrew/bin/yabai -m window --swap south
-      shift + alt - k : /opt/homebrew/bin/yabai -m window --swap north
-      shift + alt - h : /opt/homebrew/bin/yabai -m window --swap west
-      shift + alt - l : /opt/homebrew/bin/yabai -m window --swap east
+      shift + alt - j : ${yabaiBin} -m window --swap south
+      shift + alt - k : ${yabaiBin} -m window --swap north
+      shift + alt - h : ${yabaiBin} -m window --swap west
+      shift + alt - l : ${yabaiBin} -m window --swap east
 
-      ctrl + alt - j : /opt/homebrew/bin/yabai -m window --warp south
-      ctrl + alt - k : /opt/homebrew/bin/yabai -m window --warp north
-      ctrl + alt - h : /opt/homebrew/bin/yabai -m window --warp west
-      ctrl + alt - l : /opt/homebrew/bin/yabai -m window --warp east
+      ctrl + alt - j : ${yabaiBin} -m window --warp south
+      ctrl + alt - k : ${yabaiBin} -m window --warp north
+      ctrl + alt - h : ${yabaiBin} -m window --warp west
+      ctrl + alt - l : ${yabaiBin} -m window --warp east
 
-      shift + alt - p : /opt/homebrew/bin/yabai -m window --space prev
-      shift + alt - n : /opt/homebrew/bin/yabai -m window --space next
+      shift + alt - p : ${yabaiBin} -m window --space prev
+      shift + alt - n : ${yabaiBin} -m window --space next
 
-      shift + alt - 0x12 : /opt/homebrew/bin/yabai -m window --space 1
-      shift + alt - 0x13 : /opt/homebrew/bin/yabai -m window --space 2
-      shift + alt - 0x14 : /opt/homebrew/bin/yabai -m window --space 3
-      shift + alt - 0x15 : /opt/homebrew/bin/yabai -m window --space 4
-      shift + alt - 0x17 : /opt/homebrew/bin/yabai -m window --space 5
-      shift + alt - 0x16 : /opt/homebrew/bin/yabai -m window --space 6
-      shift + alt - 0x1A : /opt/homebrew/bin/yabai -m window --space 7
-      shift + alt - 0x1C : /opt/homebrew/bin/yabai -m window --space 8
-      shift + alt - 0x19 : /opt/homebrew/bin/yabai -m window --space 9
-      shift + alt - 0x1D : /opt/homebrew/bin/yabai -m window --space 10
+      shift + alt - 0x12 : ${yabaiBin} -m window --space 1
+      shift + alt - 0x13 : ${yabaiBin} -m window --space 2
+      shift + alt - 0x14 : ${yabaiBin} -m window --space 3
+      shift + alt - 0x15 : ${yabaiBin} -m window --space 4
+      shift + alt - 0x17 : ${yabaiBin} -m window --space 5
+      shift + alt - 0x16 : ${yabaiBin} -m window --space 6
+      shift + alt - 0x1A : ${yabaiBin} -m window --space 7
+      shift + alt - 0x1C : ${yabaiBin} -m window --space 8
+      shift + alt - 0x19 : ${yabaiBin} -m window --space 9
+      shift + alt - 0x1D : ${yabaiBin} -m window --space 10
     '';
   };
 }
--- nix/home.nix
+++ nix/home.nix
@@ -18,7 +18,7 @@
   home.file = {
     ".config/neofetch/config.conf".source = ./dotfiles/misc/neofetch.conf;
     ".config/yabai/yabairc".source        = ./dotfiles/misc/yabai.conf;
-    ".config/yadm/bootstrap".source       = ./dotfiles/misc/yadm-bootstrap.conf;
+    ".config/yadm/bootstrap".source       = ./dotfiles/misc/yadm-bootstrap.sh;
     ".config/yadm/encrypt".source         = ./dotfiles/misc/yadm-encrypt.conf;
     ".qutebrowser/blocked-hosts".source   = ./dotfiles/private/qutebrowser-blocked-hosts;
     ".qutebrowser/quickmarks".source      = ./dotfiles/private/qutebrowser-quickmarks;
